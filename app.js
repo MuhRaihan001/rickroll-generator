aa3f9f3ec86b5927f6000ef62d2f076ca03d21c2
MuhRaihan001 <muhraihannurtaufiq@gmail.com>
2025-04-06 09:09:57 +0800

    Dynamic load api route

--- app.js
+++ app.js
@@ -5,36 +5,16 @@ const fs = require("fs");
 const { loadUrl } = require("./src/load-url");
 const { addUrl } = require("./src/add-url");
 const { getUrlList } = require("./src/list-url");
+const { loadAPIs } = require("./src/load-api");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-loadUrl(app);
-app.post("/add-url", async (req, res) => {
-    try {
-        const { name } = req.body;
-        if (!name) return res.status(400).send("Name is required.");
-        const formatName = name.replace(/[^a-zA-Z0-9]/g, '');
-        const result =  await addUrl(formatName);
-        res.status(result.status).send(result);
-    }
-    catch (error) {
-        console.error("Error adding URL:", error);
-        res.status(500).send("Failed to add URL. Due to an internal error.");
-    }
-});
 
-app.get('/list', async (req, res) =>{
-    try{
-        const data = await getUrlList();
-        res.status(data.status).send(data);
-    }catch(error){
-        console.error("Error listing URLs:", error);
-        res.status(500).send("Failed to list URLs. Due to an internal error.");
-    }
-})
+loadUrl(app);
+loadAPIs(app);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
