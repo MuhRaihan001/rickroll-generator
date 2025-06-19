acc8d560f9a07df541551a8a02a83909697b8497
MuhRaihan001 <muhraihannurtaufiq@gmail.com>
2025-06-19 23:05:44 +0800

    fix url not leaded when first added

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cors = require("cors");
 const { loadUrl } = require("./src/load-url");
-const { loadAPIs } = require("./src/load-api");
+const { addUrl } = require("./src/add-url");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.post('/api/url', async (req, res) =>{
+    try{
+        const { name, description } = req.body;
+        if (!name) return res.status(400).send("Name is required.");
+        const formatName = name.replace(/[^a-zA-Z0-9]/g, '');
+        const result =  await addUrl(formatName, description);
+
+
+        if (result.status === 200) {
+            loadUrl(app);
+        }
+
+        res.status(result.status).send(result);
+    }catch(error){
+        console.error("Error adding URL:", error);
+        res.status(500).send("Failed to add URL. Due to an internal error.");
+    }
+});
+
 loadUrl(app);
-loadAPIs(app);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
+
+module.exports = app;
